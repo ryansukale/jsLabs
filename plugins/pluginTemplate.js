f723c8898615b01ef65bc966590709827f77622c
Ryan Sukale <ryantechnical@gmail.com>
2011-05-30 22:33:26 +0530

    Added the GPL License to all the widgets and plugins. Also deleted the bubble widget and plugin as it is renamed to the jquery.message widget

--- plugins/pluginTemplate.js
+++ plugins/pluginTemplate.js
@@ -1,3 +1,9 @@
+/**
+* Copyright 2011 Ryan Sukale
+* This program is distributed under the terms of the GNU General Public License Version 3.
+* Please check license.txt for details or log on to http://www.gnu.org/licenses/gpl.html
+*/
+
 (function($){
 
 	jQuery.fn.pluginName=function(params){
